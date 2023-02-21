631103cdcab20e957c4f82251994e219911b1f26
SHREYA <123317004+shrey-111@users.noreply.github.com>
2023-02-21 21:54:47 +0530

    Add files via upload

---
+++ inhertance.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Dog{
+public:int age;
+string colour;
+string petname;
+void run() {
+    cout<<"running"<<endl;
+}
+void bark() {
+    cout<<"barking"<<endl;
+}
+};
+class Lion{
+    public:int age;
+    string colour;
+    string petname;
+    void roar(){
+        cout<<"roaring"<<endl;
+    }
+    void run(){
+        cout<<"running"<<endl;
+    }
+};
